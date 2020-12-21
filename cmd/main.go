c122f827d3aa8dc2044bb5e733848132ac5855ea
Vlad Zaionchkovskiy <zayonvx@gmail.com>
2020-12-21 22:38:20 +0300

    build database

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"github.com/zayonvx/todo-app"
 	"github.com/zayonvx/todo-app/pkg/handler"
+	"github.com/zayonvx/todo-app/pkg/repository"
+	"github.com/zayonvx/todo-app/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
